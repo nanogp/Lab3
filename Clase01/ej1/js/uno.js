fce82ab32b69ea844844adea17b7e074e9b64f9c
Antonella Marcolin <capacita_mecon@SERVIDORES.MECON.AR>
2019-04-25 19:55:43 -0300

    clase 1

--- Clase01/ej1/js/uno.js
+++ Clase01/ej1/js/uno.js
@@ -1,5 +1,7 @@
+window.addEventListener("load", cambiaImagen);
+
 function cambiarColor(nombreColor) {
-    var h1 = document.getElementById('miH1');
+    var h1 = document.getElementById("miH1");
 
     h1.style.color = nombreColor;
 }
@@ -7,3 +9,20 @@ function cambiarColor(nombreColor) {
 function sorpresa() {
     alert("PUTO EL QUE LEE");
 }
+
+
+function cambiaImagen() {
+    //var imagen = document.getElementsByTagName("img")[1];
+    var imagen = document.getElementById("imagen-google");
+
+    imagen.addEventListener("mouseover", function (e) {
+        e.target.setAttribute("src", "https://images.clarin.com/collections/static/logo_clarin.svg");
+    });
+
+    imagen.addEventListener("mouseleave", function (e) {
+        e.target.setAttribute("src", "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png");
+    })
+}
+
+
+
