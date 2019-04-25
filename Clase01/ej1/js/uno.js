255848ef89322f4c6600d2c5ca9cd8b2b73163b1
Antonella Marcolin <capacita_mecon@SERVIDORES.MECON.AR>
2019-04-25 19:10:44 -0300

    clase 1

---
+++ Clase01/ej1/js/uno.js
@@ -0,0 +1,9 @@
+function cambiarColor(nombreColor) {
+    var h1 = document.getElementById('miH1');
+
+    h1.style.color = nombreColor;
+}
+
+function sorpresa() {
+    alert("PUTO EL QUE LEE");
+}
